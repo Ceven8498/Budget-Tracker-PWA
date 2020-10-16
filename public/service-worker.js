7fba040359ed922944d98ff253df58d282e4aa3d
CEven98 <doomfire98@gmail.com>
2020-10-15 23:38:21 -0500

    testing per

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,26 @@
-const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+
+
+const CACHE_NAME = 'my-site-cache';
+const DATA_CACHE_NAME = 'data-cache';
 
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/manifest.json",
-    "/assets/js/db.js",
-    "/assets/css/styles.css",
-    "/assets/js/index.js",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-  ];
-  
- 
-  self.addEventListener('install', function(evt) {
+  '/',
+  '/index.html',
+  '/js/idb.js',
+  '/js/index.js',
+  '/manifest.json',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', function(evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log('Your files were pre-cached successfully!');
@@ -76,3 +83,4 @@ self.addEventListener('fetch', function(evt) {
         })
       );
 });
+
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const apiRoutes = require("./routes/api.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -16,14 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useFindAndModify: false,
+  useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-app.use(apiRoutes);
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
