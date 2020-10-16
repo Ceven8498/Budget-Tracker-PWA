8b3e53fcae396c5d8e6572db82be1e1ad338654c
CEven98 <doomfire98@gmail.com>
2020-10-15 20:27:12 -0500

    test per

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,66 +10,68 @@ const FILES_TO_CACHE = [
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   ];
   
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-// install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
-  self.skipWaiting();
-});
-
-// activate
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  self.addEventListener("install", (evt) => {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (evt) => {
+    // remove old caches
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", (evt) => {
+    //GET requests to the API
+    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                // Watch response if ok, store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(() => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
       );
-    })
-  );
-
-  self.clients.claim();
-});
-
-// fetch
-self.addEventListener("fetch", evt => {
-    if(evt.request.url.includes('/api/')) {
-        console.log('[Service Worker] Fetch(data)', evt.request.url);
-    
-evt.respondWith(
-                caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200){
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            })
-            );
-            return;
-        }
-
-evt.respondWith(
-    caches.open(CACHE_NAME).then( cache => {
-      return cache.match(evt.request).then(response => {
+  
+      // stop execution of the fetch event callback
+      return;
+    }
+  
+    // "offline-first" approach.
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
         return response || fetch(evt.request);
-      });
-    })
-  );
-});
+      })
+    );
+  });
