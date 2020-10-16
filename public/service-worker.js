fda2e477bc041fd85af93cc6b3778cf43bd3b878
CEven98 <doomfire98@gmail.com>
2020-10-15 22:55:34 -0500

    testing per

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,70 +9,69 @@ const FILES_TO_CACHE = [
     "/assets/js/db.js",
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   ];
   
  
-  self.addEventListener("install", (evt) => {
+  self.addEventListener('install', function(evt) {
     evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-  
     self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (evt) => {
-    // remove old caches
+})
+
+
+self.addEventListener('activate', function(evt) {
     evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        );
-      })
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
-  
     self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", (evt) => {
-    //GET requests to the API
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+})
+
+
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(evt.request)
+                .then(response => {
+                  if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                  }
+    
+                  return response;
+                })
+                .catch(err => {
+                  return cache.match(evt.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
       evt.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then((cache) => {
-            return fetch(evt.request)
-              .then((response) => {
-                // Watch response if ok, store it in the cache.
-                if (response.status === 200) {
-                  cache.put(evt.request, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(() => {
-                // Network request failed, try to get it from the cache.
-                return cache.match(evt.request);
-              });
-          })
-          .catch((err) => console.log(err))
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get('accept').includes('text/html')) {
+              return caches.match('/');
+            }
+          });
+        })
       );
-  
-      // stop execution of the fetch event callback
-      return;
-    }
-  
-    // "offline-first" approach.
-    evt.respondWith(
-      caches.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      })
-    );
-  });
+});
